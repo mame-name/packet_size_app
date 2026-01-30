33d9914b384592cd994bff580e3b0f6ecccf3e4d
mame-name <makaya.aj@gmail.com>
2026-01-30 09:36:33 +0900

    Update app.py

--- app.py
+++ app.py
@@ -25,6 +25,7 @@ st.markdown("""
 
 def main():
     with st.sidebar:
+        st.subheader("📁 実績データ読込")
         uploaded_file = st.file_uploader("実績XLSM読込", type=['xlsm'], label_visibility="collapsed")
         st.divider()
         result_container = st.container()
